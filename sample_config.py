79831e6e2189123f8481bc7a216eed59769f1555
Khanarman1234 <135990290+Khanarman1234@users.noreply.github.com>
2023-08-21 08:31:27 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,19 +4,19 @@ import time
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6097049592:AAFqoIoUpFNiiXr7YrIk5uY0g_b5SVLMmsE")
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 28645948))
 
 
     # Get from my.telegram.org (or @UseTGXBot)
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "0d1cca3a9f4f4beb7ae8508f05ec4fcd")
     
     
     # Database URL from https://cloud.mongodb.com/
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://ainul99050:575751ai@cluster0.ozzd6ua.mongodb.net/?retryWrites=true&w=majority")
 
 
     # Your database name from mongoDB
@@ -24,7 +24,7 @@ class Config(object):
 
 
     # ID of users that can use the bot commands
-    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "6412641300").split())
 
 
     # To save user details (Usefull for getting userinfo and total user counts)
@@ -35,13 +35,13 @@ class Config(object):
 
     # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
     # To check dyno status
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "b69a94e4-895e-4156-ac93-1dbd1ff977c9")
 
 
     # OPTIONAL - To set alternate BOT COMMANDS
-    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
+    ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "filter")
     DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
-    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delall")
+    DELETE_ALL_CMD = os.environ.get("DELETE_ALL_CMDD", "delete")
     CONNECT_COMMAND = os.environ.get("CONNECT_COMMANDD", "connect")
     DISCONNECT_COMMAND = os.environ.get("DISCONNECT_COMMANDD", "disconnect")
 
